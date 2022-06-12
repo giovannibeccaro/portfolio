2d6bd8d76f0866821e4d4e69b3e77240d86add17
giovannibeccaro <gio.beccaro@gmail.com>
2022-06-12 23:26:24 +0200

    end of day changes

--- components/HomeSection.tsx
+++ components/HomeSection.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import { Icon } from "@iconify/react";
+import Logo from "./svgs/Logo";
 
 const HomeSection = () => {
-  return <div>HomeSection</div>;
+  return (
+    <div>
+      <Logo darktheme={false} />
+      <Icon icon="ci:menu-alt-05" />
+      <h1>designing and creating beautiful experiences</h1>
+      <button>CONTACT ME</button>
+      <p>or</p>
+    </div>
+  );
 };
 
 export default HomeSection;
