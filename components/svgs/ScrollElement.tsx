e4228eb801fa07811be05d3edf26df712691a18b
giovannibeccaro <gio.beccaro@gmail.com>
2022-06-13 23:00:58 +0200

    fix

--- components/svgs/ScrollElement.tsx
+++ components/svgs/ScrollElement.tsx
@@ -4,12 +4,12 @@ const ScrollElement = () => {
   return (
     <svg
       className="scroll-element"
-      viewBox="0 0 13 26"
+      viewBox="0 0 14 27"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="0.5" y="0.5" width="12" height="25" rx="6" stroke="#303030" />
-      <circle cx="6.7" cy="6.7" r="2.3" fill="#303030" />
+      <rect x="0.5" y="0.5" width="13" height="25" rx="6" stroke="#303030" />
+      <circle cx="7.1" cy="7.1" r="2.5" fill="#303030" />
     </svg>
   );
 };
