70c12a6493160e1bc4450271470e533b350a5065
giovannibeccaro <gio.beccaro@gmail.com>
2022-06-13 19:34:40 +0200

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,23 +26,23 @@ const Navbar = () => {
           <Icon icon={`${isMenuOpen ? "carbon:close" : "ci:menu-alt-05"}`} />
         </button>
         <ul className={`${isMenuOpen ? "active" : "not-active"}`}>
-          <li>
+          <li onClick={() => setIsMenuOpen(false)}>
             <a href="#">Home</a>
           </li>
-          <li>
+          <li onClick={() => setIsMenuOpen(false)}>
             <a href="#projects">Projects</a>
           </li>
-          <li>
+          <li onClick={() => setIsMenuOpen(false)}>
             <a href="#skills">skills</a>
           </li>
-          <li>
+          <li onClick={() => setIsMenuOpen(false)}>
             <a href="#about-me">about me</a>
           </li>
-          <li>
+          <li onClick={() => setIsMenuOpen(false)}>
             <a href="#contact-me">contact me</a>
           </li>
-          <li>En / It</li>
-          <li>
+          <li onClick={() => setIsMenuOpen(false)}>En / It</li>
+          <li onClick={() => setIsMenuOpen(false)}>
             <Icon icon="ic:baseline-dark-mode"></Icon>
           </li>
         </ul>
