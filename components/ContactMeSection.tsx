7bd7e38c37562c87e683b7a4501519f42ab850c0
giovannibeccaro <gio.beccaro@gmail.com>
2022-06-14 02:17:24 +0200

    animation to hero btn and contact btn

--- components/ContactMeSection.tsx
+++ components/ContactMeSection.tsx
@@ -8,31 +8,21 @@ const ContactMeSection = () => {
       <h2 className="section-heading contact-me" id="contact-me">
         Contact Me
       </h2>
-      <div data-aos="fade-up">
+      <div>
         <ContactIllustration />
       </div>
       <form>
-        <label htmlFor="name" data-aos="fade-up">
-          Full name:
-        </label>
-        <input type="text" id="name" name="name" data-aos="fade-up" />
-        <label htmlFor="email" data-aos="fade-up">
-          Email:
-        </label>
-        <input type="email" id="email" name="email" data-aos="fade-up" />
-        <label htmlFor="message" data-aos="fade-up">
-          Message:
-        </label>
-        <textarea name="message" id="message" data-aos="fade-up"></textarea>
-        <button data-aos="fade-up">Send Message</button>
+        <label htmlFor="name">Full name:</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" id="message"></textarea>
+        <button>Send Message</button>
       </form>
       <div className="contact-links">
-        <a
-          href="mailto:gio.beccaro@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-          data-aos="fade-up"
-        >
+        <p>Other contacts:</p>
+        <a href="mailto:gio.beccaro@gmail.com" target="_blank" rel="noreferrer">
           <Icon className="icon-link" icon="entypo:mail" />{" "}
           gio.beccaro@gmail.com
         </a>
@@ -40,7 +30,6 @@ const ContactMeSection = () => {
           href="https://github.com/giovannibeccaro"
           target="_blank"
           rel="noreferrer"
-          data-aos="fade-up"
         >
           <Icon className="icon-link" icon="akar-icons:github-fill" />{" "}
           giovannibeccaro
@@ -49,7 +38,6 @@ const ContactMeSection = () => {
           href="https://www.linkedin.com/in/giovanni-beccaro-133305175"
           target="_blank"
           rel="noreferrer"
-          data-aos="fade-up"
         >
           <Icon
             className="icon-link"
@@ -58,11 +46,7 @@ const ContactMeSection = () => {
           Giovanni Beccaro
         </a>
       </div>
-      <a
-        href="assets/resume.pdf"
-        download="giovanni-beccaro-resume.pdf"
-        data-aos="fade-up"
-      >
+      <a href="assets/resume.pdf" download="giovanni-beccaro-resume.pdf">
         <button className="download-link">
           <Icon className="btn-icon" icon="entypo:download" /> Download Resume
         </button>
